906d885afbc7ae250543cd8da0bf5dc1326dafce
Akash kumar <78096363+Akash-kumar-2021@users.noreply.github.com>
2023-07-27 10:29:58 +0530

    website

---
+++ src/utils/helpers.js
@@ -0,0 +1,17 @@
+// utils.js
+
+export const formatPrice = (number) => {
+  return new Intl.NumberFormat('en-IN', {
+    style: 'currency',
+    currency: 'INR',
+  }).format(number);
+};
+
+export const getUniqueValues = (data, type) => {
+  let unique = data.map((item) => item[type]);
+  if (type === 'colors') {
+    unique = unique.flat();
+  }
+
+  return ['all', ...new Set(unique)];
+};
